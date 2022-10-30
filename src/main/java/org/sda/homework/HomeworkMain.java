6a49aee1e5030014666c0a8551e19fbda395c371
carolin randala <carolinrandala@gmail.com>
2022-10-30 13:03:36 +0200

    changes

---
+++ src/main/java/org/sda/homework/HomeworkMain.java
@@ -0,0 +1,26 @@
+package org.sda.homework;
+
+public class HomeworkMain {
+        public static void main(String[] args) {
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.setName("Rectangle");
+            rectangle.setArea(40);
+            System.out.println(rectangle.getName());
+            System.out.println(rectangle.getArea());
+
+            Circle circle = new Circle();
+            circle.setName("Circle");
+            circle.setSize(30);
+            System.out.println(circle.getName());
+            System.out.println(circle.getSize());
+
+
+
+
+
+
+        }
+    }
+
+
