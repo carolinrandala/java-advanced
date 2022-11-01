a371ef63298f3f35fc315fc09b6086584eb7a17f
carolin randala <carolinrandala@gmail.com>
2022-11-01 18:44:12 +0200

    changes made

--- src/main/java/org/sda/Application.java
+++ src/main/java/org/sda/Application.java
@@ -3,25 +3,28 @@ package org.sda;
  * Write an application that will read the input and print back value that the user provided,
  * use try-catch statements to parse the input, e.g. I/O:
  */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Application {
-
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        float b = scanner.nextFloat();
-        System.out.println("Enter a integer, double or String: ");
         try {
-            if (scanner.hasNextInt()) {
-                System.out.println("int ->" + a);
-            } else if (scanner.hasNextFloat()) {
-                System.out.println("double ->" + b);
-            } else {
-                System.out.println("Hey! That's not a value! Try once more.");
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            displayNumber();
+        } catch (InputMismatchException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+    private static void displayNumber() {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            System.out.println("int -> " + a);
+        } else if (scanner.hasNextDouble()) {
+            double d = scanner.nextDouble();
+            System.out.println("double ->" + d);
+        } else {
+            throw new InputMismatchException("Hey, it's not a value! Try once more.");
         }
     }
 }
