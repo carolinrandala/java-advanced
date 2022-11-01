0ec57cf6db5d2e28af039b8b13a7d12685236a87
carolin randala <carolinrandala@gmail.com>
2022-11-01 14:26:52 +0200

    bug fix

--- src/main/java/org/sda/Application.java
+++ src/main/java/org/sda/Application.java
@@ -3,42 +3,28 @@ package org.sda;
  * Write an application that will read the input and print back value that the user provided,
  * use try-catch statements to parse the input, e.g. I/O:
  */
-
-
 import java.util.Scanner;
-
-
 public class Application {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome!");
-        int a;
-        float b;
-        String input;
-        input = scanner.nextLine();
-
-        System.out.println("Please enter a integer, double or String value: ");
+        int a = scanner.nextInt();
+        float b = scanner.nextFloat();
+        System.out.println("Enter a integer, double or String: ");
         try {
-            a = scanner.nextInt();
-            System.out.println("int ->" + a);
-            return;
-        } catch (NumberFormatException e) {
+            if (scanner.hasNextInt()) {
+                System.out.println("int ->" + a);
+            } else if (scanner.hasNextFloat()) {
+                System.out.println("double ->" + b);
+            } else {
+                System.out.println("Hey! That's not a value! Try once more.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        try {
-            b = scanner.nextFloat();
-            System.out.println("double ->" + b);
-            return;
-        } catch (NumberFormatException e) {
-        }
-        try {
-            input = String.valueOf(input);
-            System.out.println(input);
-        } catch (NumberFormatException e) {
-            System.out.println("Hey! That's not a value! Try once more.");
-        }
-
-
     }
-
-
 }
+
+
+
