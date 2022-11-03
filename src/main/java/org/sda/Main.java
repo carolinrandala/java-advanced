c8ae854ad21880c9a23b19be94bef1845cac2084
carolin randala <carolinrandala@gmail.com>
2022-11-03 20:00:31 +0200

    map exercise made

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -3,20 +3,17 @@ package org.sda;
 import org.sda.generics.*;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
         // GENERIC TYPE
         Fruit fruit = new Fruit();
         fruit.setName("Apple");
         fruit.setColor("Green");
-        fruit.setPrice(BigDecimal.valueOf(1.99)); // converting a double to BigDecimal
+        fruit.setPrice(BigDecimal.valueOf(1.99)); //converting a double to BigDecimal
 
-        GenericFood<Fruit> genericFoodWithFruit = new GenericFood<>(fruit); //assign the fruit to genericFood
+        GenericFood<Fruit> genericFoodWithFruit = new GenericFood<>(fruit);
         System.out.println(genericFoodWithFruit.getItem());
 
         String str = "I'm a great developer!";
@@ -26,38 +23,39 @@ public class Main {
         genericFoodWithString.setItem("I'm a good person!");
         System.out.println(genericFoodWithString.getItem());
 
-        // Generic type - extends
+        //Generic type - extends
         Vegetable vegetable = new Vegetable();
         GenericExtendFood<Vegetable> vegetableGenericExtendFood = new GenericExtendFood<>(vegetable);
         System.out.println(vegetableGenericExtendFood.getRating());
 
-        // Generic type - interface
+
+        //Generic type - interface
         Snack snack1 = new Snack(5);
         Snack snack2 = new Snack(3);
         System.out.println("Snack 1 is better than Snack 2: " + snack1.isBetter(snack2));
 
-        // List
+        //LIST
         List<String> animalList = new ArrayList<>();
-        animalList.add("Lion"); // index is 0
+        animalList.add("Lion"); // 0
         animalList.add("Tiger"); // 1
         animalList.add("Dog"); // 2
         animalList.add("Cat");
         animalList.add("Bear");
         animalList.add("Panda");
-        System.out.println(animalList.get(1)); //prints Tiger
+
+        System.out.println(animalList.get(1)); // prints tiger
 
         animalList.remove(1); // tiger will be deleted
         animalList.remove("Dog");
 
-        //Loop for list
-        for(String animal: animalList) {
+        for (String animal : animalList) {
             System.out.println(animal);
         }
 
         Iterator<String> stringIterator = animalList.listIterator();
-        
-        while (stringIterator.hasNext()) {
-            System.out.println(stringIterator.next() + ", ");
+
+        while(stringIterator.hasNext()) {
+            System.out.println(stringIterator.next() + " ,");
         }
 
         // List of items to be removed
@@ -67,9 +65,64 @@ public class Main {
 
         animalList.removeAll(removeAnimalList);
 
-        // Final Removed List
-        for(String animal: animalList) {
+        //Final Removed list
+        for (String animal : animalList) {
             System.out.println(animal);
         }
+
+
+        //SET
+        Set<String> countrySet = new HashSet<>(); // Non-sorted, randomly stored
+        countrySet.add("Eesti");    // 525632
+        countrySet.add("Saksmaa");  // 152635
+        countrySet.add("Poola");
+        countrySet.add("Rootsi");
+        // countrySet.add("Eesti"); -> Duplicates not allowed!
+
+        for (String country: countrySet) {
+            System.out.println(country);
+        }
+
+        System.out.println("Before sorting: " + countrySet);
+        TreeSet<String> countryTreeSet = new TreeSet<>(countrySet); // Stored as Sorted
+        System.out.println("After sorting: " + countryTreeSet);
+
+
+
+        //MAP
+        Map<String, String> fullName = new HashMap<>(); //not stored as sorted
+
+        fullName.put("Vinod", "John");
+        fullName.put("Martti", "Triksberg");
+        fullName.put("Helena", "Pinsel");
+        fullName.put("Marko", "Piir");
+
+        System.out.println(fullName); // helena=pinsel, marko=piir etc
+
+        System.out.println(fullName.get("Vinod")); // john
+        System.out.println(fullName.remove("Marko"));
+        System.out.println(fullName);
+
+        Map<String, Integer> ageMap = new HashMap<>();
+        ageMap.put("Maria", 30);
+        ageMap.put("Kaisa", 26);
+        ageMap.put("Martin", 21);
+        System.out.println(ageMap);
+
+       //Map of list
+        Map<String, List<String>> friendsMap = new HashMap<>();
+        List<String> vinodFriendsList = List.of("Tony", "Martin", "Kelly");
+        List<String> mariaFriendsList = List.of("Eva", "Kaisa", "Tiina");
+        friendsMap.put("Vinod", vinodFriendsList);
+        friendsMap.put("Maria", mariaFriendsList);
+
+       //Map of map
+        Map<String, Map<String, String>> detailsMap = new HashMap<>();
+        Map<String, String> vinodInfoMap = new HashMap<>();
+        vinodInfoMap.put("age", "15");
+        vinodInfoMap.put("residence", "Estonia");
+        vinodInfoMap.put("Phone", "732882");
+        detailsMap.put("Vinod", vinodInfoMap);
+
     }
 }
