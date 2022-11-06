4eae4d7e36c2275bbc14b205a1a81d9bad556fed
carolin randala <carolinrandala@gmail.com>
2022-11-06 18:02:31 +0200

    bug fix

---
+++ src/main/java/org/sda/homework/HomeworkColors.java
@@ -0,0 +1,17 @@
+package org.sda.homework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 1. Create a set consisting of colors - given from the user.
+ * 2. Present the removal of individual elements from the set.
+ * 3. Display the collection before and after sorting.
+ */
+public class HomeworkColors {
+    public static void main(String[] args) {
+
+        Set<String> colorsSet = new HashSet<>();
+
+    }
+}
