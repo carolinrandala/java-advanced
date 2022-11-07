29c84e26f82877ceb8b1b840a929b6594b71dd2c
carolin randala <carolinrandala@gmail.com>
2022-11-07 21:05:29 +0200

    Lambda examples added

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -2,14 +2,46 @@ package org.sda;
 
 import org.sda.model.Person;
 
-import java.util.function.Predicate;
+import java.util.Random;
+import java.util.function.*;
 
 public class Main {
+    //Lambda expression
+    //predicate
     public static void main(String[] args) {
-        //LAMBDA EXPRESSION
-        //PREDICATE
-       //Person person = new Person("Carolin", "Randala", 25);
-       // Predicate<Person> personTest = test -> test.getAge() > 20;
-       // System.out.println(personTest.test(person));
+        Person person = new Person("Carolin", "Randala", 20);
+        Predicate<Person> personTest = test -> test.getAge() > 18; //lambda expression
+        System.out.println(personTest.test(person));
+
+        //New way of writing method : Functional Interface
+        Function<String, Integer> getStringLen = s -> {
+            int increment = 10;
+            return s.length() + increment;
+        };
+
+        System.out.println(getStringLen.apply("Java is not easy. Joke ;)"));
+
+        //Method reference
+        Predicate<Person> personTest2 = Person::isAdult; //Method reference
+        System.out.println(personTest2.test(person));
+
+        //Supplier: Doesn't take any arguments but returns/supplies only the value
+        Supplier<Integer> randomNumberSupplier = () -> new Random().nextInt(); //No argument but returns random value
+        System.out.println(randomNumberSupplier.get()); //get() should be called always
+
+        //Consumer: It takes an argument, do the operations  and returns nothing
+        Consumer<Person> printPerson = p -> System.out.println("Person first name: " + p.getFirstName() + " last name: " + p.getLastName());
+        printPerson.accept(person); //accept() should be called always
+
+        //Operator
+        UnaryOperator<Integer> toSquare = i -> i * i;
+        System.out.println(toSquare.apply(4)); //apply() should be called always
     }
+
+    //Old-school way of method definition
+    private static Integer getLengthOfString(String inputString) {
+        int increment = 10;
+        return inputString.length() + increment;
+    }
+
 }
--- src/main/java/org/sda/lambda/AdultPersonTest.java
+++ src/main/java/org/sda/lambda/AdultPersonTest.java
@@ -1,7 +1,13 @@
 package org.sda.lambda;
 
+import org.sda.model.Person;
+
 import java.util.function.Predicate;
 
-public class AdultPersonTest {
+public class AdultPersonTest implements Predicate<Person> {
 
+    @Override
+    public boolean test(Person person) {
+        return person.getAge() >= 18;
+    }
 }
